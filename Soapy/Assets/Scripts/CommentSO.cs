649def38552febaae2feba204ef9c2123d3a9413
GhassanNaja05 <gassnaja@gmail.com>
2025-01-23 19:51:58 -0500

    core mechanics almost done

--- Soapy/Assets/Scripts/CommentSO.cs
+++ Soapy/Assets/Scripts/CommentSO.cs
@@ -6,14 +6,26 @@ using UnityEngine;
 
 public class CommentSO : ScriptableObject
 {
+	public enum SpecialTriggers {none, scientistUpgrade, mainStoryline, trueEnding}
+	
 	public enum Stats {money, popularity, quality, government}
 	
+	public SpecialTriggers specialTrigger = SpecialTriggers.none;
+	[Space]
+	public string userName;
+	[Space]
 	[TextArea(10, 14)] public string comment;
 	public Sprite profile;
 	[Space]
-	public Stats[] affectedVariablesLike;
-	public int[] affectedValuesLike;
-	[Space]
-	public Stats[] affectedVariablesDislike;
-	public int[] affectedValuesDislike;
+	[Header("Affected stats when liking")]
+	public float moneyL = 0;
+	public float governmentL = 0;
+	public float popularityL = 0;
+	public float qualityL = 0;
+	
+	[Header("Affected stats when disliking")]
+	public float moneyD = 0;
+	public float governmentD = 0;
+	public float popularityD = 0;
+	public float qualityD = 0;
 }
